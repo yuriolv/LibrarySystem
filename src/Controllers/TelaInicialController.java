1cd112c7bae9610a47351a607019e118032ae072
DiogoChaves1 <diogochaves.software@gmail.com>
2023-06-20 21:18:03 -0300

    melhorando design Tela Inicial

--- src/Controllers/TelaInicialController.java
+++ src/Controllers/TelaInicialController.java
@@ -1,9 +1,26 @@
 package Controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TelaInicialController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class TelaInicialController implements Initializable {
+
+    @FXML
+    private AnchorPane acervoPane;
+
+    @FXML
+    private AnchorPane bibliotecarioPane;
+
+    @FXML
+    private AnchorPane usuariosPane;
+
+    
 
     @FXML
     void redirecionarTelaAcervo(MouseEvent event) {
@@ -17,6 +34,13 @@ public class TelaInicialController {
     @FXML
     void redirecionarTelaUsers(MouseEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        acervoPane.setCursor(Cursor.HAND);
+        bibliotecarioPane.setCursor(Cursor.CLOSED_HAND);
+        usuariosPane.setCursor(Cursor.CLOSED_HAND);
     }
 
 }
