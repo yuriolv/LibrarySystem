cc74fc95ef9932474a283ad1268bad9bded4d333
yuriolv <yuri_2505@icloud.com>
2023-06-24 16:54:41 -0300

    atualizando layout das telas de login

--- src/Controllers/loginUserController.java
+++ src/Controllers/loginUserController.java
@@ -1,56 +1,21 @@
 package Controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.ToggleGroup;
 
 public class loginUserController {
     @FXML
-    private RadioButton docente;
-    @FXML 
-    private RadioButton discente;
+    private ToggleGroup typeGroup;
     @FXML 
     private TextField user;
 
 
     @FXML 
-    public void handleDocente(MouseEvent e){
-        boolean selectDocente = docente.isSelected();
-        
-        if(selectDocente){
-            discente.setSelected(false);
-        }
-
-    }
-
-    @FXML 
-    public void handleDiscente(MouseEvent e){
-        boolean selectDiscente = discente.isSelected();
-
-        if(selectDiscente){
-            docente.setSelected(false);
-        }
-
-    }
-
-    @FXML 
     public void verifyLoginUser(){
-        String typeSelected = getTypeUser();
+        String typeSelected = typeGroup.getSelectedToggle().toString();
         String userField = user.getText().toString();
         
     }
 
-    public String getTypeUser(){
-        boolean selectDocente = docente.isSelected();
-        boolean selectDiscente = discente.isSelected();
-
-        if(selectDiscente)
-            return "discente";
-
-        else if(selectDocente)
-            return "docente";
-
-            return "";
-    }
 }
