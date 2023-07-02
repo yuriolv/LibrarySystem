a5e95e97e751e78d64153ea7a93b480893e70808
renan lima <renanrod2403@gmail.com>
2023-07-02 12:20:01 -0300

    anchor pane dinâmico adicionado

--- src/Controllers/RentBookController.java
+++ src/Controllers/RentBookController.java
@@ -1,8 +1,13 @@
 package Controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
+import Classes.Livro;
 import Classes.User;
+import Models.Livros;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -10,6 +15,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class RentBookController{
@@ -21,8 +28,12 @@ public class RentBookController{
     private Label labelSair;
     @FXML
     private Label matriculaLabel;
+    @FXML
+    private FlowPane flowpane;
 
     private User user;
+
+    private ArrayList<Livro> livros;
 
     @FXML
     void changePageUser(MouseEvent event) throws IOException {
@@ -61,5 +72,28 @@ public class RentBookController{
         matriculaLabel.setText(user.getMatricula());    
     }
 
+    public AnchorPane createAnchorPane(String bookName, String bookAuthor) {
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setPrefWidth(179);
+        anchorPane.setPrefHeight(241);
+
+        Label name = new Label(bookName);
+        Label author = new Label(bookAuthor);
+
+        anchorPane.getChildren().addAll(name, author);
+        return anchorPane;
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+        Livros crud = new Livros();
+
+        livros = new ArrayList<Livro>();
+        crud.read(livros);
+
+        for (Livro livro : livros) {
+            AnchorPane bookCard = createAnchorPane(livro.getTitulo(), livro.getAutor());
+            flowpane.getChildren().add(bookCard);
+        }
+    }
 
 }
